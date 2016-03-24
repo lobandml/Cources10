4136b81ed06b9cc2be3ea4f5d9a4c6ab445f4fdb
lobandml <loband_ml@ukr.net>
2016-03-24 20:41:09 +0200

    первые два задания

---
+++ Cources10(IO)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cources10_IO_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tasks.Task1(100);
+            Tasks.Task2();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cources10(IO)/Tasks.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Cources10_IO_
+{
+    class Tasks
+    {
+        public static void Task1(int countOfFolders)
+        {
+            string prefix = "Folder_";
+
+            for (int i = 0; i < countOfFolders; i++)
+            {
+                Directory.CreateDirectory(prefix + i.ToString());
+            }
+            for (int i = 0; i < countOfFolders; i++)
+            {
+                Directory.Delete(prefix + i.ToString());
+            }
+        }
+        public static void Task2()
+        {
+            string path = "TestFile.txt";
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.WriteLine("Съешь ещё этих мягких французских булок,");
+                    writer.WriteLine("да выпей же чаю.");
+                }
+            }
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string str;
+                while (!sr.EndOfStream)
+                {
+                    //str = sr.ReadLine();
+                   str= sr.ReadToEnd();
+                   Console.WriteLine(str);
+                    //Console.WriteLine(str);
+                }
+            }
+
+            
+        }
+        public static void Task3()
+        {
+
+        }
+    }
+}
